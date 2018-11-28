5c30cf955cc6385a5268e369bc6eed85f31f42d6
solin <shlsolin@163.com>
2018-11-29 00:34:49 +0800

    添加UserCenter模块

--- App/src/main/java/com/solin/mall/ui/activity/MainActivity.kt
+++ App/src/main/java/com/solin/mall/ui/activity/MainActivity.kt
@@ -1,7 +1,8 @@
-package com.solin.mall
+package com.solin.mall.ui.activity
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import com.solin.mall.R
 
 class MainActivity : AppCompatActivity() {
 
